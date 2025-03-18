b15bc898b8cf6a9c95287dbfc34637846ca1802a
angiez37 <angelsaka@yahoo.com>
2025-03-17 22:33:12 -0400

    Directions enum outlining various drone headings. closes #2

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Explorer.java
@@ -15,10 +15,7 @@ import ca.mcmaster.se2aa4.island.teamXXX.Handler;
 import ca.mcmaster.se2aa4.island.teamXXX.Test;
 public class Explorer implements IExplorerRaid {
     private Drone drone;
-    private Island island;
-    private Handler handler;
-    //private Test test;
-
+  
     private final Logger logger = LogManager.getLogger();
 
     @Override
@@ -26,33 +23,20 @@ public class Explorer implements IExplorerRaid {
         logger.info("** Initializing the Exploration Command Center");
         JSONObject info = new JSONObject(new JSONTokener(new StringReader(s)));
 
-        this.handler = new Handler();
-        //this.test = new Test();
-
-
         logger.info("** Initialization info:\n {}",info.toString(2));
         Direction heading = Direction.toDirection(info.getString("heading"));
         Integer batteryLevel = info.getInt("budget");
         this.drone = new Drone(batteryLevel, heading);
-        this.island = new Island(drone);
         logger.info("The drone is facing {}", heading);
         logger.info("Battery level is {}", batteryLevel);
     }
 
     @Override
     public String takeDecision() {
-        JSONObject decision = this.handler.makeDecision(this.drone, this.island);
-        logger.info("** Decision: {}", decision.toString());
-
-        //decision.put("action", "fly"); // we stop the exploration immediately
-        //logger.info("** Decision: {}",decision.toString());
-        
-        return decision.toString();
-
-        //JSONObject decision = new JSONObject();
-        //decision.put("action", "scan");
-        //logger.info("** Decision: {}",decision.toString());
-        //return decision.toString();
+        JSONObject decision = new JSONObject();
+         decision.put("action", "stop"); // we stop the exploration immediately
+         logger.info("** Decision: {}",decision.toString());
+         return decision.toString();
     }
 
     @Override
@@ -65,10 +49,6 @@ public class Explorer implements IExplorerRaid {
         logger.info("The status of the drone is {}", status);
         JSONObject extraInfo = response.getJSONObject("extras");
         logger.info("Additional information received: {}", extraInfo);
-        Data data = new Data(this.handler.getCommand(), cost, extraInfo, Boolean.parseBoolean(status));
-        this.island.update(data);
-        this.drone.updateBattery(data.getCost());
-        logger.info("The battery level is now: {}", this.drone.getBattery());
     }
 
     @Override
