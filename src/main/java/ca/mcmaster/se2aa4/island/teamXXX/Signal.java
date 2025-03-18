36b5fc312383615342b6074cc26083e06e3c3973
angiez37 <angelsaka@yahoo.com>
2025-03-17 22:51:11 -0400

    signal enum outlining possible echo results

---
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Signal.java
@@ -0,0 +1,14 @@
+package ca.mcmaster.se2aa4.island.teamXXX;
+
+public enum Signal {
+    GROUND,
+    OCEAN,
+    OUTOFRANGE,
+    UNKNOWN;
+
+    @Override
+    public String toString() {
+        return name(); // Returns the name of the enum constant as a String
+    }
+}
+
