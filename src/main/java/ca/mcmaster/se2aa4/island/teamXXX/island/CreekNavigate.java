4161b5b2b24c37e196e39415d6ae625b9feb9326
Sachika <saxens20@mcmaster.ca>
2025-03-24 21:05:43 -0400

    Refactored creekNavigate class (#11)

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/island/CreekNavigate.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/island/CreekNavigate.java
@@ -1,14 +1,12 @@
 package ca.mcmaster.se2aa4.island.teamXXX.island;
-import ca.mcmaster.se2aa4.island.teamXXX.drone.Drone;
-import ca.mcmaster.se2aa4.island.teamXXX.island.Island;
-import ca.mcmaster.se2aa4.island.teamXXX.Handler;
-import ca.mcmaster.se2aa4.island.teamXXX.island.Signal;
-import ca.mcmaster.se2aa4.island.teamXXX.drone.Commands;
-import ca.mcmaster.se2aa4.island.teamXXX.drone.Direction;
-
-import org.json.JSONObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
+
+import ca.mcmaster.se2aa4.island.teamXXX.Handler;
+import ca.mcmaster.se2aa4.island.teamXXX.drone.Commands;
+import ca.mcmaster.se2aa4.island.teamXXX.drone.Direction;
+import ca.mcmaster.se2aa4.island.teamXXX.drone.Drone;
 
 
 
@@ -40,7 +38,50 @@ public class CreekNavigate implements Navigate {
     }
 
     public JSONObject search(Drone drone, Island island, Handler handler) {
-        if(clockwiseSearch){     
+        if(clockwiseSearch){  
+            JSONObject echoNavigate = echo(drone, handler);
+            if (echoNavigate != null) return echoNavigate;
+
+
+            JSONObject groundNavigate = forwardUntilGround(drone, island, handler);
+            if (groundNavigate != null) return groundNavigate;
+
+           
+
+            JSONObject edgeStop = stopEdgeIsland(drone, island, handler);
+            if (edgeStop != null) return edgeStop;
+
+            JSONObject skiptoLand = skipWater(drone, island, handler);
+            if (skiptoLand != null) return skiptoLand;
+
+
+
+            JSONObject prepareTurnR = prepareForRightTurn(drone, island, handler);
+            if (prepareTurnR != null) return prepareTurnR;
+
+            JSONObject turningEast = turnEast(drone, island, handler);
+            if (turningEast != null) return turningEast;
+          
+
+            JSONObject prepareTurnL = prepareForLeftTurn(drone, island, handler);
+            if (prepareTurnL != null) return prepareTurnL;
+
+            JSONObject turningWest = turnWest(drone, island, handler);
+            if (turningWest != null) return turningWest;
+
+           
+            //if none of the conditions are met
+            logger.info("drone stopping");
+            handler.setCommand(Commands.STOP);
+            return drone.stop();
+        }
+        
+        return handler.makeDecision(drone, island);
+    }
+
+
+
+    public JSONObject echo(Drone drone, Handler handler) {
         if (iteration == 0) {
             iteration++;
             handler.setCommand(Commands.ECHOFORWARD);
@@ -53,8 +94,12 @@ public class CreekNavigate implements Navigate {
             iteration++;
             handler.setCommand(Commands.ECHOLEFT);
             return drone.echoLeft();
+        }
+        return null; 
+    }
 
-        } else if (island.getForward() == Signal.GROUND && island.getForwardRange() == 0) {
+    public JSONObject forwardUntilGround(Drone drone, Island island, Handler handler) {
+        if (island.getForward() == Signal.GROUND && island.getForwardRange() == 0) {
             if (forwardIteration == 0) {
                 forwardIteration++;
                 handler.setCommand(Commands.FLY);
@@ -65,7 +110,26 @@ public class CreekNavigate implements Navigate {
                 handler.setCommand(Commands.SCAN);
                 return drone.scan();
             }
-        } else if (island.getForward() == Signal.GROUND && island.getForwardRange() > 0) {
+        }
+        return null; 
+    }
+
+    public JSONObject stopEdgeIsland(Drone drone, Island island, Handler handler){
+        if(island.getForward() == Signal.OUTOFRANGE && ((island.getRight() == Signal.OUTOFRANGE && island.getRightRange() < 1) || (island.getLeft() == Signal.OUTOFRANGE && island.getLeftRange() < 1))) {
+            logger.info("drone reached the end of the island");
+            handler.setCommand(Commands.STOP);
+            return drone.stop();
+        }
+
+        return null; 
+
+
+        
+
+    }
+
+    public JSONObject skipWater(Drone drone, Island island, Handler handler){
+        if (island.getForward() == Signal.GROUND && island.getForwardRange() > 0) {
             if (forwardIteration == 0) {
                 forwardIteration++;
                 handler.setCommand(Commands.SCAN);
@@ -81,15 +145,15 @@ public class CreekNavigate implements Navigate {
                 handler.setCommand(Commands.SCAN);
                 return drone.scan();
             }
-        } else if(island.getForward() == Signal.OUTOFRANGE && ((island.getRight() == Signal.OUTOFRANGE && island.getRightRange() < 1) || (island.getLeft() == Signal.OUTOFRANGE && island.getLeftRange() < 1))) {
-            logger.info("drone reached the end of the island");
-            handler.setCommand(Commands.STOP);
-            return drone.stop();
-        }else if ((drone.getHeading() == Direction.EAST && turnIteration == 0 && island.getForward() == Signal.OUTOFRANGE && island.getRight() == Signal.GROUND && island.getRightRange() == 0 && emptyFrontIteration == 0)) {
+        } 
+        return null;
+    }
+
+    public JSONObject prepareForRightTurn(Drone drone, Island island, Handler handler){
+        if ((drone.getHeading() == Direction.EAST && turnIteration == 0 && island.getForward() == Signal.OUTOFRANGE && island.getRight() == Signal.GROUND && island.getRightRange() == 0 && emptyFrontIteration == 0)) {
             logger.info("flying before turning from east to west");
             if (forwardIteration == 0) {
                 forwardIteration++;
-               // iteration = 0;
                 handler.setCommand(Commands.FLY);
                 return drone.fly();
             } else if (forwardIteration == 1) {
@@ -98,95 +162,23 @@ public class CreekNavigate implements Navigate {
                 handler.setCommand(Commands.SCAN);
                 return drone.scan();
             }
-        } else if ((drone.getHeading() == Direction.EAST && turnIteration == 0 && island.getForward() == Signal.OUTOFRANGE && island.getRightRange() > 0 && emptyFrontIteration == 0)) {
+        }
+  
+        if ((drone.getHeading() == Direction.EAST && turnIteration == 0 && island.getForward() == Signal.OUTOFRANGE && island.getRightRange() > 0 && emptyFrontIteration == 0)) {
             logger.info("turning from east to west");
             emptyFrontIteration = 1;
-        } else if (island.getForward() == Signal.OUTOFRANGE && turnIteration == 0){
-            if (forwardIteration == 0) {
-                forwardIteration++;
-                handler.setCommand(Commands.TURNLEFT);
-                return drone.turnLeft();
-            } else if (forwardIteration == 1) {
-                forwardIteration++;
-                handler.setCommand(Commands.FLY);
-                return drone.fly();
-            }else if (forwardIteration == 2) {
-                forwardIteration++;
-                handler.setCommand(Commands.TURNLEFT);
-                return drone.turnLeft();
-            } else if (forwardIteration == 3) {
-                forwardIteration++;
-                handler.setCommand(Commands.TURNLEFT);
-                return drone.turnLeft();
-            } else if (forwardIteration == 4) {
-                forwardIteration++;
-                handler.setCommand(Commands.TURNLEFT);
-                return drone.turnLeft();
-            } else if (forwardIteration == 5) {
-                forwardIteration++;
-                handler.setCommand(Commands.TURNRIGHT);
-                return drone.turnRight();
-            } else if (forwardIteration == 6) {
-                forwardIteration++;
-                handler.setCommand(Commands.TURNRIGHT);
-                return drone.turnRight();
-            } else if (forwardIteration == 7){
-                forwardIteration = 0;
-                turnIteration = 1;
-                iteration = 0;
-                handler.setCommand(Commands.SCAN);
-                return drone.scan();
-            }
-        } 
-        /*else if (island.getForward() == Signal.OUTOFRANGE && turnIteration == 0) {
-            if (forwardIteration == 0) {
-                forwardIteration++;
-                handler.setCommand(Commands.TURNLEFT);
-                return drone.turnLeft();
-            }  else if (forwardIteration == 1) {
-                forwardIteration++;
-                handler.setCommand(Commands.TURNRIGHT);
-                return drone.turnRight();
-            } else if (forwardIteration == 2) {
-                forwardIteration++;
-                handler.setCommand(Commands.TURNRIGHT);
-                return drone.turnRight();
-            } else if (forwardIteration == 3) {
-                forwardIteration++;
-                handler.setCommand(Commands.FLY);
-                return drone.fly();
-            } else if (forwardIteration == 4) {
-                forwardIteration++;
-                handler.setCommand(Commands.TURNRIGHT);
-                logger.info("DEBUG - No conditions met: heading={}, forward={}, right={}, left={}, ranges=[fwd={},right={},left={}], iterations=[main={},fwd={},turn={},empty={}]",
-                drone.getHeading(),
-                island.getForward(),
-                island.getRight(),
-                island.getLeft(),
-                island.getForwardRange(),
-                island.getRightRange(),
-                island.getLeftRange(),
-                iteration,
-                forwardIteration,
-                turnIteration,
-                emptyFrontIteration);
-                return drone.turnRight();
-            } else if (forwardIteration == 5) {
-                forwardIteration++;
-                handler.setCommand(Commands.FLY);
-                return drone.fly();
-            } else if (forwardIteration == 6) {
-                forwardIteration = 0;
-                turnIteration = 1;
-                iteration = 0;
-                handler.setCommand(Commands.SCAN);
-                return drone.scan();
-                */
-        else if ((drone.getHeading() == Direction.WEST && island.getForward() == Signal.OUTOFRANGE && island.getLeft() == Signal.GROUND && island.getLeftRange() == 0 && emptyFrontIteration == 1)){
+        }
+
+        return null;
+
+    }
+
+
+    public JSONObject prepareForLeftTurn(Drone drone, Island island, Handler handler){
+        if ((drone.getHeading() == Direction.WEST && island.getForward() == Signal.OUTOFRANGE && island.getLeft() == Signal.GROUND && island.getLeftRange() == 0 && emptyFrontIteration == 1)){
             logger.info("flying before turning from west to east");
             if (forwardIteration == 0) {
                 forwardIteration++;
-                //iteration = 0;
                 handler.setCommand(Commands.FLY);
                 return drone.fly();
             } else if (forwardIteration == 1) {
@@ -195,10 +187,59 @@ public class CreekNavigate implements Navigate {
                 handler.setCommand(Commands.SCAN);
                 return drone.scan();
             }
-                } else if ((drone.getHeading() == Direction.WEST && island.getForward() == Signal.OUTOFRANGE && island.getLeftRange() > 0 && emptyFrontIteration == 1)) {
+        }
+
+        if ((drone.getHeading() == Direction.WEST && island.getForward() == Signal.OUTOFRANGE && island.getLeftRange() > 0 && emptyFrontIteration == 1)) {
             logger.info("turning from west to east");
             emptyFrontIteration = 0;
-        } else if (island.getForward() == Signal.OUTOFRANGE && turnIteration == 1){
+        }
+        return null;   
+    }
+
+
+    public JSONObject turnEast(Drone drone, Island island , Handler handler){
+        if (island.getForward() == Signal.OUTOFRANGE && turnIteration == 0){
+            if (forwardIteration == 0) {
+                forwardIteration++;
+                handler.setCommand(Commands.TURNLEFT);
+                return drone.turnLeft();
+            } else if (forwardIteration == 1) {
+                forwardIteration++;
+                handler.setCommand(Commands.FLY);
+                return drone.fly();
+            } else if (forwardIteration == 2) {
+                forwardIteration++;
+                handler.setCommand(Commands.TURNLEFT);
+                return drone.turnLeft();
+            } else if (forwardIteration == 3) {
+                forwardIteration++;
+                handler.setCommand(Commands.TURNLEFT);
+                return drone.turnLeft();
+            } else if (forwardIteration == 4) {
+                forwardIteration++;
+                handler.setCommand(Commands.TURNLEFT);
+                return drone.turnLeft();
+            } else if (forwardIteration == 5) {
+                forwardIteration++;
+                handler.setCommand(Commands.TURNRIGHT);
+                return drone.turnRight();
+            } else if (forwardIteration == 6) {
+                forwardIteration++;
+                handler.setCommand(Commands.TURNRIGHT);
+                return drone.turnRight();
+            } else if (forwardIteration == 7){
+                forwardIteration = 0;
+                turnIteration = 1;
+                iteration = 0;
+                handler.setCommand(Commands.SCAN);
+                return drone.scan();
+            }
+        }
+        return null;
+    }
+
+    public JSONObject turnWest(Drone drone, Island island, Handler handler){
+        if (island.getForward() == Signal.OUTOFRANGE && turnIteration == 1){
             if (forwardIteration == 0) {
                 forwardIteration++;
                 handler.setCommand(Commands.TURNRIGHT);
@@ -207,7 +248,7 @@ public class CreekNavigate implements Navigate {
                 forwardIteration++;
                 handler.setCommand(Commands.FLY);
                 return drone.fly();
-            }else if (forwardIteration == 2) {
+            } else if (forwardIteration == 2) {
                 forwardIteration++;
                 handler.setCommand(Commands.TURNRIGHT);
                 return drone.turnRight();
@@ -233,53 +274,11 @@ public class CreekNavigate implements Navigate {
                 iteration = 0;
                 handler.setCommand(Commands.SCAN);
                 return drone.scan();
-            }/*if (forwardIteration == 0) {
-                forwardIteration++;
-                handler.setCommand(Commands.TURNRIGHT);
-                return drone.turnRight();
-            } else if (forwardIteration == 1) {
-                forwardIteration++;
-                handler.setCommand(Commands.TURNRIGHT);
-                return drone.turnRight();
-            } else if (forwardIteration == 2) {
-                forwardIteration++;
-                handler.setCommand(Commands.TURNLEFT);
-                return drone.turnLeft();
-            } else if (forwardIteration == 3) {
-                forwardIteration++;
-                handler.setCommand(Commands.TURNLEFT);
-                return drone.turnLeft();
-            } else if (forwardIteration == 4) {
-                forwardIteration++;
-                handler.setCommand(Commands.FLY);
-                return drone.fly();
-            } else if (forwardIteration == 5) {
-                forwardIteration++;
-
-                handler.setCommand(Commands.TURNLEFT);
-                return drone.turnLeft();
-            } else if (forwardIteration == 6 || forwardIteration == 7|| forwardIteration == 8) {
-                forwardIteration++;
-                handler.setCommand(Commands.FLY);
-                return drone.fly();
-            } else if(forwardIteration == 9) {
-                forwardIteration++;
-                handler.setCommand(Commands.TURNLEFT);
-                return drone.turnLeft();
-            } else if (forwardIteration == 10){
-                forwardIteration = 0;
-                turnIteration = 0;
-                iteration = 0;
-                handler.setCommand(Commands.SCAN);
-                return drone.scan();
-            }*/
-        } else {
-            logger.info("drone stopping");
-            handler.setCommand(Commands.STOP);
-            return drone.stop();
-        }
-        } 
-        return handler.makeDecision(drone, island);
-    } 
-
+            }
+    }
+    return null;
+   
 }
+    
+}
+
